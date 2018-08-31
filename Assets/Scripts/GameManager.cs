6662d6a5cbf079e158b98e4d3da3f9c81ef4bdf8
Utsav Talwar <talwar.utsav@gmail.com>
2018-08-31 21:04:57 +0530

    Added gameplay

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static bool gameEnded = false;
+
+	public void Update()
+	{
+		if (gameEnded == false) 
+		{
+			gameEnded = true;	
+			Debug.Log ("GAME OVER");
+			//Restart ();
+		}
+	}
+
+	void Restart ()
+	{	
+		SceneManager.LoadScene ("SampleScene");
+	}
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,14 +6,21 @@ public class PlayerBehaviour : MonoBehaviour {
 
 	[SerializeField] private float acceleration;
 	[SerializeField] private float maxSpeed;	
+	[SerializeField] private GameObject[] nodes;
+	private Rigidbody2D playerRigidbody;
+	int curr_node=0;
 
-	private Rigidbody2D playerRigidbody;       
-
-	void Start()
+	void Awake()
 	{
 		playerRigidbody = gameObject.GetComponent<Rigidbody2D> ();
+		for(int i = 0; i < nodes.Length; i++)
+		{
+			nodes [i].GetComponentInChildren<Light> ().enabled = false;
+			nodes [i].GetComponentInChildren<BoxCollider2D> ().enabled = false;
+		}
+		nodes [curr_node].GetComponentInChildren<BoxCollider2D>().enabled = true;
 	}
-
+			
 	void FixedUpdate()
 	{
 		float horizontalMovement = Input.GetAxis ("Horizontal");
@@ -28,7 +35,14 @@ public class PlayerBehaviour : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D node)
 	{
-		
+			nodes [curr_node].GetComponentInChildren<Light> ().enabled = true;
+		if (curr_node == nodes.Length-1) {
+			GameManager.gameEnded = true;
+			return;
+		}
+		else
+			curr_node++;
+			nodes [curr_node].GetComponentInChildren<BoxCollider2D>().enabled = true;
 	}
-}
 
+}
