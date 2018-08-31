626a6f267fab8cf77ac939b569816c7cebf2603f
Utsav Talwar <talwar.utsav@gmail.com>
2018-09-01 00:20:54 +0530

    Ädded popup

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour {
 
 	[SerializeField] private float acceleration;
 	[SerializeField] private float maxSpeed;	
 	[SerializeField] private GameObject[] nodes;
+	[SerializeField] private Image clueImage;
 	private Rigidbody2D playerRigidbody;
-	int curr_node=0;
+	int curr_node = 0;
 
 	void Awake()
 	{
+		clueImage.enabled = false;
+
 		playerRigidbody = gameObject.GetComponent<Rigidbody2D> ();
 		for(int i = 0; i < nodes.Length; i++)
 		{
@@ -19,6 +23,7 @@ public class PlayerBehaviour : MonoBehaviour {
 			nodes [i].GetComponentInChildren<BoxCollider2D> ().enabled = false;
 		}
 		nodes [curr_node].GetComponentInChildren<BoxCollider2D>().enabled = true;
+
 	}
 			
 	void FixedUpdate()
@@ -27,6 +32,12 @@ public class PlayerBehaviour : MonoBehaviour {
 		float verticalMovement = Input.GetAxis ("Vertical");
 
 		Vector2 movement = new Vector2 (horizontalMovement, verticalMovement);
+		float distance = Vector2.Distance(nodes[curr_node].transform.position, transform.position );
+		if (distance < 0.5f) {
+			clueImage.enabled = true;
+		} else {
+			clueImage.enabled = false;
+		}
 
 		playerRigidbody.AddForce(movement * acceleration);
 		playerRigidbody.velocity = Vector2.ClampMagnitude(playerRigidbody.velocity, maxSpeed);
@@ -41,6 +52,7 @@ public class PlayerBehaviour : MonoBehaviour {
 			return;
 		}
 		else
+			nodes [curr_node].GetComponentInChildren<BoxCollider2D>().enabled = false;
 			curr_node++;
 			nodes [curr_node].GetComponentInChildren<BoxCollider2D>().enabled = true;
 	}
