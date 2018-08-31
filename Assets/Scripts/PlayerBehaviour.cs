b093676c523da9b96782d37d97a466d8810dd0ce
Utsav Talwar <talwar.utsav@gmail.com>
2018-08-31 09:10:22 +0530

    Added Player Movements

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	[SerializeField] private float acceleration;
+	[SerializeField] private float maxSpeed;	
+
+	private Rigidbody2D playerRigidbody;       
+
+	void Start()
+	{
+		playerRigidbody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (horizontalMovement, verticalMovement);
+
+		playerRigidbody.AddForce(movement * acceleration);
+		playerRigidbody.velocity = Vector2.ClampMagnitude(playerRigidbody.velocity, maxSpeed);
+
+	}
+}
+
